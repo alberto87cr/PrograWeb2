9b0b4ddb5ed72c0b90b2e14ae02a4ac5674ddbe7
alberto87cr <alberto87.cr@gmail.com>
2015-04-19 21:51:33 -0600

    First Commit
    Initial Commit

---
+++ CarParkingCoRi/CarParkingCoRi/Views/Intern.master.cs
@@ -0,0 +1,89 @@
+﻿using CarParkingCoRi.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CarParkingCoRi.Views
+{
+    public partial class Intern : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loadRootPath()
+        {
+            functions fs = functions.getInstance();
+            if (fs.isNullRootPath())
+            {
+                Uri MyUrl = Request.Url;
+                fs.setRootPath(MyUrl);
+            }
+        }
+
+        protected string rootPath()
+        {
+            return functions.getInstance().getRootPath();
+        }
+
+        protected string VehiclesPath()
+        {
+            return functions.getInstance().getVehiclesPath();
+        }
+
+        protected string CustomersPath()
+        {
+            return functions.getInstance().getCustomersPath();
+        }
+
+        protected string AreasPath()
+        {
+            return functions.getInstance().getAreasPath();
+        }
+
+        protected string UsersPath()
+        {
+            return functions.getInstance().getUsersPath();
+        }
+
+        protected string HomePath()
+        {
+            return functions.getInstance().getHomePath();
+        }
+
+        protected string IndexPath()
+        {
+            return functions.getInstance().getIndexPage();
+        }
+
+        protected string AddPath()
+        {
+            return functions.getInstance().getAddPage();
+        }
+
+        protected string DeletePath()
+        {
+            return functions.getInstance().getDeletePage();
+        }
+
+        protected string ListPath()
+        {
+            return functions.getInstance().getListPage();
+        }
+
+        protected string UpdatePath()
+        {
+            return functions.getInstance().getUpdatePage();
+        }
+
+        protected string detailPath()
+        {
+            return functions.getInstance().getDetailPage();
+        }
+
+    }
+}
---
+++ CarParkingCoRi/CarParkingCoRi/Views/Intern.master.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace CarParkingCoRi.Views {
+    
+    
+    public partial class Intern {
+        
+        /// <summary>
+        /// Control HeaderIntern.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeaderIntern;
+        
+        /// <summary>
+        /// Control MainIntern.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainIntern;
+        
+        /// <summary>
+        /// Control FooterInternTop.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder FooterInternTop;
+        
+        /// <summary>
+        /// Control FooterInternBottom.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder FooterInternBottom;
+    }
+}
